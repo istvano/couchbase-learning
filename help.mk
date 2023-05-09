9db06e36d964fd79959a3a8e533c5e3d904d863c
Istvan Orban <istvan.orbann@gmail.com>
2023-05-09 18:54:12 +0200

    restructure to support different cb envs

---
+++ help.mk
@@ -0,0 +1,101 @@
+# ===SETUP
+BLUE      := $(shell tput -Txterm setaf 4)
+GREEN     := $(shell tput -Txterm setaf 2)
+TURQUOISE := $(shell tput -Txterm setaf 6)
+WHITE     := $(shell tput -Txterm setaf 7)
+YELLOW    := $(shell tput -Txterm setaf 3)
+GREY      := $(shell tput -Txterm setaf 1)
+RESET     := $(shell tput -Txterm sgr0)
+
+SMUL      := $(shell tput smul)
+RMUL      := $(shell tput rmul)
+
+ifeq ($(OS),Windows_NT)
+    CCFLAGS += -D WIN32
+    ifeq ($(PROCESSOR_ARCHITEW6432),AMD64)
+        CCFLAGS += -D AMD64
+    else
+        ifeq ($(PROCESSOR_ARCHITECTURE),AMD64)
+            CCFLAGS += -D AMD64
+        endif
+        ifeq ($(PROCESSOR_ARCHITECTURE),x86)
+            CCFLAGS += -D IA32
+        endif
+    endif
+else
+    UNAME_S := $(shell uname -s)
+    ifeq ($(UNAME_S),Linux)
+        CCFLAGS += -D LINUX
+    endif
+    ifeq ($(UNAME_S),Darwin)
+        CCFLAGS += -D OSX
+    endif
+    UNAME_P := $(shell uname -p)
+    ifeq ($(UNAME_P),x86_64)
+        CCFLAGS += -D AMD64
+    endif
+    ifneq ($(filter %86,$(UNAME_P)),)
+        CCFLAGS += -D IA32
+    endif
+    ifneq ($(filter arm%,$(UNAME_P)),)
+        CCFLAGS += -D ARM
+    endif
+endif
+
+# Variable wrapper
+define defw
+	custom_vars += $(1)
+	$(1) ?= $(2)
+	export $(1)
+	shell_env += $(1)="$$($(1))"
+endef
+
+# Variable wrapper for hidden variables
+define defw_h
+	$(1) := $(2)
+	shell_env += $(1)="$$($(1))"
+endef
+
+# Add the following 'help' target to your Makefile
+# And add help text after each target name starting with '\#\#'
+# A category can be added with @category
+HELP_FUN = \
+	%help; \
+	use Data::Dumper; \
+	while(<>) { \
+		if (/^([_a-zA-Z0-9\-\/]+)\s*:.*\#\#(?:@([a-zA-Z0-9\-\/_\s]+))?\t(.*)$$/ \
+			|| /^([_a-zA-Z0-9\-\/]+)\s*:.*\#\#(?:@([a-zA-Z0-9\-\/]+))?\s(.*)$$/) { \
+			$$c = $$2; $$t = $$1; $$d = $$3; \
+			push @{$$help{$$c}}, [$$t, $$d, $$ARGV] unless grep { grep { grep /^$$t$$/, $$_->[0] } @{$$help{$$_}} } keys %help; \
+		} \
+	}; \
+	for (sort keys %help) { \
+		printf("${WHITE}%24s:${RESET}\n\n", $$_); \
+		for (@{$$help{$$_}}) { \
+			printf("%s%25s${RESET}%s  %s${RESET}\n", \
+				( $$_->[2] eq "Makefile" || $$_->[0] eq "help" ? "${YELLOW}" : "${GREY}"), \
+				$$_->[0], \
+				( $$_->[2] eq "Makefile" || $$_->[0] eq "help" ? "${GREEN}" : "${GREY}"), \
+				$$_->[1] \
+			); \
+		} \
+		print "\n"; \
+	}
+
+
+default: help
+
+.PHONY: help
+help:: ##@Other Show this help.
+	@echo ""
+	@printf "%30s " "${BLUE}VARIABLES"
+	@echo "${RESET}"
+	@echo ""
+	@printf "${BLUE}%25s${RESET}${TURQUOISE}  ${SMUL}%s${RESET}\n" $(foreach v, $(custom_vars), $v $(if $($(v)),$($(v)), ''))
+	@echo ""
+	@echo ""
+	@echo ""
+	@printf "%30s " "${YELLOW}TARGETS"
+	@echo "${RESET}"
+	@echo ""
+	@perl -e '$(HELP_FUN)' $(MAKEFILE_LIST)
---
+++ rbac.mk
@@ -0,0 +1,90 @@
+### Rest
+
+.PHONY: rest/certs
+rest/certs: ##@rest List certs
+	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/pools/default/certificates \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD | jq
+
+### rbac
+
+.PHONY: rest/rbac/whoami
+rest/rbac/whoami: ##@rest Get whoami
+	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/whoami \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD | jq
+
+.PHONY: rest/rbac/roles
+rest/rbac/roles: ##@rest Get the roles
+	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/settings/rbac/roles \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD | jq	
+
+.PHONY: rest/rbac/permissions
+rest/rbac/permissions: ##@rest Get the permissions for current user
+	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/pools/default/checkPermissions \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD \
+		-d '$(PERMISSION)' | jq ;
+
+.PHONY: rest/rbac/users
+rest/rbac/users: ##@rest Get users
+	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/settings/rbac/users \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD | jq	
+
+.PHONY: rest/rbac/groups
+rest/rbac/groups: ##@rest Get the groups
+	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/settings/rbac/groups \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD | jq
+
+.PHONY: rest/rbac/create-user-per-roles
+rest/rbac/create-user-per-roles: ##@rest Create a user per role
+	@for v in $(ROLES) ; do \
+		echo "Creating user: $$v"; \
+		ROLE_STR="roles=$$v&password=password"; \
+		$(CURL) $(CURL_OPTS) -X PUT $(API_ENDPOINT)/settings/rbac/users/local/$$v \
+			-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD \
+			-d $$ROLE_STR; \
+	done
+	@for v in $(BUCKET_ROLES) ; do \
+		echo "Creating user: $$v"; \
+		ROLE_STR="roles=$$v[*]&password=password"; \
+		$(CURL) $(CURL_OPTS) -X PUT $(API_ENDPOINT)/settings/rbac/users/local/$$v \
+			-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD \
+			-d $$ROLE_STR; \
+	done
+	@echo "Completed..."
+
+.PHONY: rest/rbac/delete-user-per-roles
+rest/rbac/delete-user-per-roles: ##@rest Delete a user for every role
+	@for v in $(ROLES) ; do \
+		echo "Deleting user: $$v"; \
+		ROLE_STR="roles=$$v&password=password"; \
+		$(CURL) $(CURL_OPTS) -X DELETE $(API_ENDPOINT)/settings/rbac/users/local/$$v \
+			-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD; \
+	done
+	@for v in $(BUCKET_ROLES) ; do \
+		echo "Deleting user: $$v"; \
+		ROLE_STR="roles=$$v&password=password"; \
+		$(CURL) $(CURL_OPTS) -X DELETE $(API_ENDPOINT)/settings/rbac/users/local/$$v \
+			-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD; \
+	done
+	@echo "Completed..."
+
+.PHONY: rest/rbac/check-perms-all
+rest/rbac/check-perms-all: ##@rest Get user permissions for all users
+	@for v in $(ROLES) ; do \
+		echo "User: $$v"; \
+		$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/pools/default/checkPermissions \
+		-u $$v:$$COUCHBASE_PASSWORD \
+		-d '$(PERMISSION)' | jq | grep -E '{|}|true' ;\
+	done
+	@for v in $(BUCKET_ROLES) ; do \
+		echo "User: $$v"; \
+		$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/pools/default/checkPermissions \
+		-u $$v:$$COUCHBASE_PASSWORD \
+		-d '$(PERMISSION)' | jq | grep -E '{|}|true' ;\
+	done
+
+
+.PHONY: rest/rbac/check-perms
+rest/rbac/check-perms: ##@rest Check DB_USER permission against list
+	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/pools/default/checkPermissions \
+		-u $(DB_USER):$$COUCHBASE_PASSWORD \
+		-d '$(PERMISSION)' | jq

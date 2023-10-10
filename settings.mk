1133afba08dac01696d6c33fd22b64cb935a0056
Istvan Orban <istvan.orbann@gmail.com>
2023-10-10 16:35:14 +0100

    add sample data permission

--- settings.mk
+++ settings.mk
@@ -4,16 +4,16 @@ settings/saml: ##@Settings Show SAML settings
 		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD | jq -S
 
 .PHONY: settings/saml/load
-settings/saml/load: ##@Settings Load SAML settings from json
+settings/saml/load: ##@Settings Load SAML settings from json, you need to replace the key in spKey
 	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/settings/saml \
 		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD \
 		-H "Content-Type:application/json" \
 		-d @../etc/saml_settings.json
 
-
 .PHONY: settings/saml/save
 settings/saml/save: ##@Settings Save SAML settings
 	$(CURL) $(CURL_OPTS) $(API_ENDPOINT)/settings/saml \
 		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD \
-		-H "Content-Type:application/json" | jq -S \
+		-H "Content-Type:application/json" | jq \
+		'del(.spMetadataURL, .spLogoutURL, .spConsumeURL, .idpMetadataTLSExtraOpts)' \
 		>../etc/saml_settings.json

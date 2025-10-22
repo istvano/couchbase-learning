832a4327cf4c4fd555c88e977f551a6fac2c28ac
Istvan Orban <istvan.orbann@gmail.com>
2025-10-22 17:03:19 +0100

    add xdcr setup

--- xdcr.mk
+++ xdcr.mk
@@ -23,11 +23,41 @@ xdcr/init: ##@xdcr Init cluster
 	&& $(DOCKER) exec -it $(APP)_$(XDCR_NODE) \
 	./bin/couchbase-cli cluster-init \
 		-c couchbase://$$IP \
-		--cluster-name $$CLUSTER_NAME \
+		--cluster-name xdcr_cluster \
   		--cluster-username $$COUCHBASE_USERNAME \
   		--cluster-password $$COUCHBASE_PASSWORD \
-  		--services data,index,query,fts \
+  		--services $(SERVICES) \
   		--cluster-ramsize $$COUCHBASE_RAM_SIZE \
   		--cluster-index-ramsize $$COUCHBASE_INDEX_RAM_SIZE \
   		--index-storage-setting default)
-		
+
+
+.PHONY: setup/sample/import
+xdcr/sample/import: ##@xdcr Import sample data for xdcr node
+	$(DOCKER) exec -it $(APP)_$(XDCR_NODE) \
+	$(CURL) $(CURL_OPTS) -v $(API_ENDPOINT)/sampleBuckets/install \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD \
+		-d '["travel-sample"]'
+
+.PHONY: xdcr/setup/remote/create
+xdcr/setup/remote/create: ##@xdcr Import sample data for xdcr node
+	$(DOCKER) exec -it $(APP)_$(XDCR_NODE) \
+	$(CURL) $(CURL_OPTS) -v $(API_ENDPOINT)/pools/default/remoteClusters \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD \
+  		--data-raw 'name=$(CLUSTER_NAME)&hostname=$(APP)_$(MAIN_NODE)%3A8091&username=Administrator&password=password'
+
+.PHONY: xdcr/bucket/create-replicated
+xdcr/bucket/create-replicated: ##@xdcr Create 'travel-sample-replicated' bucket
+	$(DOCKER) exec -it $(APP)_$(XDCR_NODE) \
+	$(CURL) $(CURL_OPTS) -X POST $(API_ENDPOINT)/pools/default/buckets \
+		-u $$COUCHBASE_USERNAME:$$COUCHBASE_PASSWORD \
+		-H "Content-Type: application/x-www-form-urlencoded" \
+		--data-urlencode name=travel-sample-replicated \
+		--data-urlencode bucketType=couchbase \
+		--data-urlencode ramQuotaMB=256 \
+		--data-urlencode replicaNumber=1 \
+		--data-urlencode storageBackend=couchstore \
+		--data-urlencode flushEnabled=0 \
+		--data-urlencode evictionPolicy=valueOnly \
+		--data-urlencode durabilityMinLevel=none \
+		--data-urlencode conflictResolutionType=seqno
